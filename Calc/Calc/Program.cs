1958af333005844b65d4fba56cf0c8453fd79fef
IVSHAH <shashovivan2@mail.ru>
2023-06-01 12:01:57 +0300

    Калькулятор

---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Перове число:");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите оператор (+,-,*,/):*");
+char op = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Второе число:");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double res = 0;
+switch (op)
+{
+    case'+':
+        res = x1 + x2;
+        break;
+    case '-':
+        res = x1 - x2;
+        break;
+    case '*':
+        res = x1 * x2;
+        break;
+    case '/':
+        res = x1 / x2;
+        break;
+    default:
+        Console.WriteLine("Неверный оператор");
+        break;
+}
+Console.WriteLine("Результат:" + res);
+
