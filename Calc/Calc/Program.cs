e2d9fc6b0cd4715a6ba55c90f40a81c6c8e3386d
IVSHAH <shashovivan2@mail.ru>
2023-06-01 12:27:59 +0300

    Код для возведения в степень

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Перове число:");
 double x1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите оператор (+,-,*,/):*");
+Console.WriteLine("Введите оператор (+,-,*,/,^):*");
 char op = Convert.ToChar(Console.ReadLine());
 Console.WriteLine("Второе число:");
 double x2 = Convert.ToDouble(Console.ReadLine());
@@ -18,6 +18,9 @@ switch (op)
         break;
     case '/':
         res = x1 / x2;
+        break;
+    case '^':
+        res = Math.Pow(x1, x2);
         break;
     default:
         Console.WriteLine("Неверный оператор");
