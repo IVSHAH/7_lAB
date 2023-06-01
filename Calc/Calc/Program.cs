92fefca132c3213a23ea286aed0361ed32d7dd71
IVSHAH <shashovivan2@mail.ru>
2023-06-01 13:20:33 +0300

    Завершение программы

--- Calc/Calc/Program.cs
+++ Calc/Calc/Program.cs
@@ -1,31 +1,34 @@
-﻿Console.WriteLine("Здравствуйте!");
-Console.WriteLine("Перове число:");
-double x1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите оператор (+,-,*,/,^):*");
-char op = Convert.ToChar(Console.ReadLine());
-Console.WriteLine("Второе число:");
-double x2 = Convert.ToDouble(Console.ReadLine());
-double res = 0;
-switch (op)
+﻿do
 {
-    case'+':
-        res = x1 + x2;
-        break;
-    case '-':
-        res = x1 - x2;
-        break;
-    case '*':
-        res = x1 * x2;
-        break;
-    case '/':
-        res = x1 / x2;
-        break;
-    case '^':
-        res = Math.Pow(x1, x2);
-        break;
-    default:
-        Console.WriteLine("Неверный оператор");
-        break;
-}
-Console.WriteLine("Результат:" + res);
-
+    Console.WriteLine("Здравствуйте!");
+    Console.WriteLine("Перове число:");
+    double x1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите оператор (+,-,*,/,^):*");
+    char op = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine("Второе число:");
+    double x2 = Convert.ToDouble(Console.ReadLine());
+    double res = 0;
+    switch (op)
+    {
+        case '+':
+            res = x1 + x2;
+            break;
+        case '-':
+            res = x1 - x2;
+            break;
+        case '*':
+            res = x1 * x2;
+            break;
+        case '/':
+            res = x1 / x2;
+            break;
+        case '^':
+            res = Math.Pow(x1, x2);
+            break;
+        default:
+            Console.WriteLine("Неверный оператор");
+            break;
+    }
+    Console.WriteLine("Результат:" + res);
+    Console.WriteLine("Завершить работу программы?(yes/no)");
+} while (Console.ReadLine().ToUpper() == "yes");
